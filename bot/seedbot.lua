69724bb69d33478fcf6f62d60b7fd316f570a774
jeiboor <antispam0099@Gmail.com>
2016-03-20 10:20:04 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {160890821,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -235,21 +235,17 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+@MR_saf سازنده
+@Aryanes81 ادمین 
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+
+MRsaf
+Mohammad mafya
+Mahdi TFB
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+Persian : @telesafch
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -321,6 +317,8 @@ This command will send text to [group_id]
 » Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 
 » Only owner can use res,setowner,promote,demote and log commands
+
+سازنده : @MR_saf
 
 ]],
     help_text = [[
@@ -427,6 +425,7 @@ Will return group ban list
 
 » Only owner can use res,setowner,promote,demote and log commands
 
+سازنده : @MR_saf
 ]]
   }
   serialize_to_file(config, './data/config.lua')
